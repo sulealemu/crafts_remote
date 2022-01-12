8e87c7de94a72db502933a15e4a74c74eff26c98
sulealemu <suleman4edu@gmail.com>
2022-01-12 20:11:21 +0300

    commited

---
+++ HelloWorldproj/src/com/pack1/Example1.java
@@ -0,0 +1,14 @@
+
+
+package com.pack1;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Out put of the Class Example1");
+System.out.println("Out put of the Class Example1");
+
+	}
+
+}
