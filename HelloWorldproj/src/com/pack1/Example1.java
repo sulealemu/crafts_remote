16140a895cc3139ff7a0391dd2fc6e111f78cb88
sulealemu <suleman4edu@gmail.com>
2022-01-12 20:16:28 +0300

    commited from java project

--- HelloWorldproj/src/com/pack1/Example1.java
+++ HelloWorldproj/src/com/pack1/Example1.java
@@ -7,7 +7,6 @@ public class Example1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Out put of the Class Example1");
-System.out.println("Out put of the Class Example1");
 
 	}
 
